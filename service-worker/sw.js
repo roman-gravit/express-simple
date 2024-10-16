10be668e1edfee9f9501458040ba124a2af7336d
Roman Gravit <romang@siber.com>
2024-10-16 01:31:27 +0100

    Add wasm simple test and Service Worker

---
+++ service-worker/sw.js
@@ -0,0 +1,28 @@
+let counter = 0;
+
+self.oninstall = (event) => {
+  console.log("установка сервисного исполнителя");
+};
+
+self.onactivate = (event) => {
+  console.log("активация сервисного исполнителя");
+  event.waitUntil(self.clients.claim());
+};
+
+self.onfetch = (event) => {
+  console.log("fetch", event.request.url);
+  if (event.request.url.endsWith("/data.json")) {
+    counter++;
+    event.respondWith(
+      new Response(JSON.stringify({ counter }), {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+    );
+    return;
+  }
+
+  event.respondWith(fetch(event.request));
+
+};
